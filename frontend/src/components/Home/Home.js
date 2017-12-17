6fcdb410c7bd36fba20a2408ee9ed49db3abf646
tommy_mfg <tommy.wu@madeforgoods.com.cn>
2017-12-17 13:28:26 +0800

    Update

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -11,6 +11,11 @@ const customerStatisticsLogo = require('../../images/customer-general-icon.png')
 const goodStatisticsLogo = require('../../images/good-statistics-icon.png');
 const consumerStatisticsLogo = require('../../images/consumer-statistics-icon.png');
 
+const homeBottom1 = require('../../images/homepage-bottom4.png');
+const homeBottom2 = require('../../images/homepage-bottom3.png');
+const homeBottom3 = require('../../images/homepage-bottom2.png');
+const homeBottom4 = require('../../images/homepage-bottom1.png');
+
 const Home = class extends Component {
 	constructor(props) {
 		super(props);
@@ -42,29 +47,52 @@ const Home = class extends Component {
 						</div>
 					</div> */}
 					<div ref="appClickable" className="main-board-app">
-						<div className="app" align="center">
-							<Link to="/CustomerStatistics" style={{ textDecoration: 'none' }}>
-								<img className="icon" src={customerStatisticsLogo} alt="" />
-								<span className="app-text font-app-title">
+						<div className='app-box'>
+							<div className="app" align="center">
+								<Link to="/CustomerStatistics" style={{ textDecoration: 'none' }}>
+									<img className="icon" src={customerStatisticsLogo} alt="" />
+									<span className="app-text font-app-title">
 									{customerStatisticsText}
 								</span>
-							</Link>
-						</div>
-						<div className="app" align="center">
-							<Link to="/goodStatistics" style={{ textDecoration: 'none' }}>
-								<img className="icon" src={goodStatisticsLogo} alt="" />
-								<span className="app-text font-app-title">
+								</Link>
+							</div>
+							<div className="app" align="center">
+								<Link to="/goodStatistics" style={{ textDecoration: 'none' }}>
+									<img className="icon" src={goodStatisticsLogo} alt="" />
+									<span className="app-text font-app-title">
 									{goodStatisticsText}
 								</span>
-							</Link>
-						</div>
-						<div className="app" align="center">
-							<Link to="/consumerStatistics" style={{ textDecoration: 'none' }}>
-								<img className="icon" src={consumerStatisticsLogo} alt="" />
-								<span className="app-text font-app-title">
+								</Link>
+							</div>
+							<div className="app" align="center">
+								<Link to="/consumerStatistics" style={{ textDecoration: 'none' }}>
+									<img className="icon" src={consumerStatisticsLogo} alt="" />
+									<span className="app-text font-app-title">
 									{consumerStatisticsText}
 								</span>
-							</Link>
+								</Link>
+							</div>
+						</div>
+					</div>
+					<div className="main-board-bottom">
+						<div className="main-board-box">
+							<div className="normal-head font-app-title">常用的页面</div>
+							<div className="normal font-content-link clearfix">
+								<img src={homeBottom1}/>
+								<span>最近数据</span>	
+							</div>
+							<div className="normal font-content-link clearfix">
+								<img src={homeBottom2}/>
+								<span>门店成员</span>
+							</div>
+							<div className="normal font-content-link clearfix">
+								<img src={homeBottom3}/>
+								<span>库存</span>
+							</div>
+							<div className="normal font-content-link clearfix">
+								<img src={homeBottom4}/>
+								<span>窜货</span>
+							</div>
 						</div>
 					</div>
 				</div>
